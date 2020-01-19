430029d4ca128ac36c29dc225db88d213f8aaa3b
Amory Meltzer <Amorymeltzer@gmail.com>
2020-01-19 06:05:34 -0500

    Update eslint to 6.8.0 from 5.16.0
    https://github.com/azatoth/twinkle/commit/d2496cc5054aa821305032aa7447fb9e1e028f5d

--- modules/twinkleprotect.js
+++ modules/twinkleprotect.js
@@ -1480,7 +1480,7 @@ TwinkleGlobal.protect.callbacks = {
 		}
 
 		var newtag = '=== [[:' + MorebitsGlobal.pageNameNorm + ']] ===\n';
-		if ((new RegExp('^' + RegExp.escape(newtag).replace(/\s+/g, '\\s*'), 'm')).test(text)) {
+		if (new RegExp('^' + RegExp.escape(newtag).replace(/\s+/g, '\\s*'), 'm').test(text)) {
 			statusElement.error([ 'There is already a protection request for this page at ', rppLink, ', aborting.' ]);
 			return;
 		}
