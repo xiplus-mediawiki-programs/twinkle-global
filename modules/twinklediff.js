xiplus-mediawiki-programs/twinkle-global

commit 736d3ba61d97f1738585c0fde667ea2fe289aec3

morebits: remove Morebits.queryString
https://github.com/azatoth/twinkle/commit/37d6b670d38d2b3c6060490ad71eda5410561508

Morebits.queryString is redundant to mw.util.getParamValue(),
mw.util.getUrl() and jQuery.param(). All existing usages are easily
replaced.
Specifically,
- Morebits.queryString.get() and Morebits.queryString.exists() are
redundant to mw.util.getParamValue()
- Morebits.queryString.create() is redundant to mw.util.getUrl() and
jQuery.param()

While queryString can be also used for constructing query string objects
and getting/setting param values in them, this functionality is not used
anywhere,
nor are there any reasonable potential usecases.

Co-authored-by: Siddharth VP <siddharthvp@gmail.com>
Co-authored-by: Amory Meltzer <Amorymeltzer@gmail.com>

modules/twinklediff.js
@@ -16,39 +16,19 @@ TwinkleGlobal.diff = function twinklediff() {
 	if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
 		return;
 	}
-
-	var disabledWikis = $.map(TwinkleGlobal.getPref('diffDisabledWikis'), function(el) {
-		return el.value.trim();
-	});
-
-	if (disabledWikis.indexOf(mw.config.get('wgDBname')) !== -1) {
-		return;
-	}
-
-	var query = {
-		'title': mw.config.get('wgPageName'),
-		'diff': 'cur',
-		'oldid': 'prev'
-	};
-
-	TwinkleGlobal.addPortletLink(mw.util.wikiScript('index') + '?' + $.param(query), 'Last', 'twg-lastdiff', 'Show most recent diff');
+	TwinkleGlobal.addPortletLink(mw.util.getUrl(mw.config.get('wgPageName'), {diff: 'cur', oldid: 'prev'}), 'Last', 'tw-lastdiff', 'Show most recent diff');
 
 	// Show additional tabs only on diff pages
-	if (MorebitsGlobal.queryString.exists('diff')) {
+	if (mw.util.getParamValue('diff')) {
 		TwinkleGlobal.addPortletLink(function() {
 			TwinkleGlobal.diff.evaluate(false);
-		}, 'Since', 'twg-since', 'Show difference between last diff and the revision made by previous user');
+		}, 'Since', 'tw-since', 'Show difference between last diff and the revision made by previous user');
 		TwinkleGlobal.addPortletLink(function() {
 			TwinkleGlobal.diff.evaluate(true);
-		}, 'Since mine', 'twg-sincemine', 'Show difference between last diff and my last revision');
+		}, 'Since mine', 'tw-sincemine', 'Show difference between last diff and my last revision');
 
 		var oldid = /oldid=(.+)/.exec($('#mw-diff-ntitle1').find('strong a').first().attr('href'))[1];
-		query = {
-			'title': mw.config.get('wgPageName'),
-			'diff': 'cur',
-			'oldid': oldid
-		};
-		TwinkleGlobal.addPortletLink(mw.util.wikiScript('index') + '?' + $.param(query), 'Current', 'twg-curdiff', 'Show difference to current revision');
+		TwinkleGlobal.addPortletLink(mw.util.getUrl(mw.config.get('wgPageName'), {diff: 'cur', oldid: oldid}), 'Current', 'tw-curdiff', 'Show difference to current revision');
 	}
 };
 
@@ -89,12 +69,10 @@ TwinkleGlobal.diff.callbacks = {
 			self.statelem.error('no suitable earlier revision found, or ' + self.params.user + ' is the only contributor. Aborting.');
 			return;
 		}
-		var query = {
-			'title': mw.config.get('wgPageName'),
-			'oldid': revid,
-			'diff': mw.config.get('wgCurRevisionId')
-		};
-		window.location = mw.util.wikiScript('index') + '?' + MorebitsGlobal.queryString.create(query);
+		window.location = mw.util.getUrl(mw.config.get('wgPageName'), {
+			diff: mw.config.get('wgCurRevisionId'),
+			oldid: revid
+		});
 	}
 };
 })(jQuery);

modules/friendlywelcome.js
@@ -14,8 +14,8 @@
  */
 
 TwinkleGlobal.welcome = function friendlywelcome() {
-	if (MorebitsGlobal.queryString.exists('friendlywelcome')) {
-		if (MorebitsGlobal.queryString.get('friendlywelcome') === 'auto') {
+	if (mw.util.getParamValue('friendlywelcome')) {
+		if (mw.util.getParamValue('friendlywelcome') === 'auto') {
 			TwinkleGlobal.welcome.auto();
 		} else {
 			TwinkleGlobal.welcome.semiauto();
@@ -26,7 +26,7 @@ TwinkleGlobal.welcome = function friendlywelcome() {
 };
 
 TwinkleGlobal.welcome.auto = function() {
-	if (MorebitsGlobal.queryString.get('action') !== 'edit') {
+	if (mw.util.getParamValue('action') !== 'edit') {
 		// userpage not empty, aborting auto-welcome
 		return;
 	}
@@ -39,7 +39,7 @@ TwinkleGlobal.welcome.semiauto = function() {
 };
 
 TwinkleGlobal.welcome.normal = function() {
-	if (MorebitsGlobal.queryString.exists('diff')) {
+	if (mw.util.getParamValue('diff')) {
 		// check whether the contributors' talk pages exist yet
 		var $oList = $('#mw-diff-otitle2').find('span.mw-usertoollinks a.new:contains(talk)').first();
 		var $nList = $('#mw-diff-ntitle2').find('span.mw-usertoollinks a.new:contains(talk)').first();
@@ -63,7 +63,7 @@ TwinkleGlobal.welcome.normal = function() {
 				var oHref = $oList.attr('href');
 
 				var oWelcomeNode = welcomeNode.cloneNode(true);
-				oWelcomeNode.firstChild.setAttribute('href', oHref + '&' + MorebitsGlobal.queryString.create({
+				oWelcomeNode.firstChild.setAttribute('href', oHref + '&' + $.param({
 					'friendlywelcome': TwinkleGlobal.getPref('quickWelcomeMode') === 'auto' ? 'auto' : 'norm',
 					'vanarticle': MorebitsGlobal.pageNameNorm
 				}));
@@ -75,7 +75,7 @@ TwinkleGlobal.welcome.normal = function() {
 				var nHref = $nList.attr('href');
 
 				var nWelcomeNode = welcomeNode.cloneNode(true);
-				nWelcomeNode.firstChild.setAttribute('href', nHref + '&' + MorebitsGlobal.queryString.create({
+				nWelcomeNode.firstChild.setAttribute('href', nHref + '&' + $.param({
 					'friendlywelcome': TwinkleGlobal.getPref('quickWelcomeMode') === 'auto' ? 'auto' : 'norm',
 					'vanarticle': MorebitsGlobal.pageNameNorm
 				}));
@@ -97,7 +97,7 @@ TwinkleGlobal.welcome.welcomeUser = function welcomeUser() {
 
 	var params = {
 		value: TwinkleGlobal.getPref('quickWelcomeTemplate'),
-		article: MorebitsGlobal.queryString.exists('vanarticle') ? MorebitsGlobal.queryString.get('vanarticle') : '',
+		article: mw.util.getParamValue('vanarticle') || '',
 		mode: 'auto'
 	};
 
@@ -147,7 +147,7 @@ TwinkleGlobal.welcome.callback = function friendlywelcomeCallback(uid) {
 		type: 'input',
 		name: 'article',
 		label: '* Linked article (if supported by template):',
-		value: MorebitsGlobal.queryString.exists('vanarticle') ? MorebitsGlobal.queryString.get('vanarticle') : '',
+		value: mw.util.getParamValue('vanarticle') || '',
 		tooltip: 'An article might be linked from within the welcome if the template supports it. Leave empty for no article to be linked.  Templates that support a linked article are marked with an asterisk.'
 	});
 

modules/twinklebatchdelete.js
@@ -127,10 +127,10 @@ TwinkleGlobal.batchdelete.callback = function twinklebatchdeleteCallback() {
 	} else if (mw.config.get('wgCanonicalSpecialPageName') === 'Prefixindex') {
 
 		query.generator = 'allpages';
-		query.gaplimit = TwinkleGlobal.getPref('batchMax'); // the max for sysops
-		if (MorebitsGlobal.queryString.exists('prefix')) {
-			query.gapnamespace = MorebitsGlobal.queryString.get('namespace');
-			query.gapprefix = MorebitsGlobal.string.toUpperCaseFirstChar(MorebitsGlobal.queryString.get('prefix'));
+		query.gaplimit = TwinkleGlobal.getPref('batchMax');
+		if (mw.util.getParamValue('prefix')) {
+			query.gapnamespace = mw.util.getParamValue('namespace');
+			query.gapprefix = MorebitsGlobal.string.toUpperCaseFirstChar(mw.util.getParamValue('prefix'));
 		} else {
 			var pathSplit = decodeURIComponent(location.pathname).split('/');
 			if (pathSplit.length < 3 || pathSplit[2] !== 'Special:PrefixIndex') {

modules/twinklebatchprotect.js
@@ -280,10 +280,9 @@ TwinkleGlobal.batchprotect.callback = function twinklebatchprotectCallback() {
 		query.gcmlimit = TwinkleGlobal.getPref('batchMax'); // the max for sysops
 	} else if (mw.config.get('wgCanonicalSpecialPageName') === 'Prefixindex') {
 		query.generator = 'allpages';
-		query.gapnamespace = MorebitsGlobal.queryString.exists('namespace') ? MorebitsGlobal.queryString.get('namespace') : $('select[name=namespace]').val();
-		query.gapprefix = MorebitsGlobal.queryString.exists('from') ? MorebitsGlobal.string.toUpperCaseFirstChar(MorebitsGlobal.queryString.get('from').replace('+', ' ')) :
-			MorebitsGlobal.string.toUpperCaseFirstChar($('input[name=prefix]').val());
-		query.gaplimit = TwinkleGlobal.getPref('batchMax'); // the max for sysops
+		query.gapnamespace = mw.util.getParamValue('namespace') || $('select[name=namespace]').val();
+		query.gapprefix = MorebitsGlobal.string.toUpperCaseFirstChar(mw.util.getParamValue('from') ? mw.util.getParamValue('from').replace('+', ' ') : $('input[name=prefix]').val());
+		query.gaplimit = TwinkleGlobal.getPref('batchMax');
 	} else {
 		query.generator = 'links';
 		query.titles = mw.config.get('wgPageName');

modules/twinklefluff.js
@@ -28,7 +28,7 @@ TwinkleGlobal.fluff = function twinklefluff() {
 			'SineBot'
 		];
 
-		if (MorebitsGlobal.queryString.exists('twinklerevert')) {
+		if (mw.util.getParamValue('twinklerevert')) {
 			// Return if the user can't edit the page in question
 			if (!mw.config.get('wgIsProbablyEditable')) {
 				alert("Unable to edit the page, it's probably protected.");
@@ -80,7 +80,7 @@ TwinkleGlobal.fluff.auto = function twinklefluffauto() {
 
 	var vandal = $('#mw-diff-ntitle2').find('a.mw-userlink').text();
 
-	TwinkleGlobal.fluff.revert(MorebitsGlobal.queryString.get('twinklerevert'), vandal, true);
+	TwinkleGlobal.fluff.revert(mw.util.getParamValue('twinklerevert'), vandal, true);
 };
 
 TwinkleGlobal.fluff.addLinks = {
@@ -108,11 +108,11 @@ TwinkleGlobal.fluff.addLinks = {
 					var href = $(current).find('.mw-changeslist-diff').attr('href');
 					current.appendChild(document.createTextNode(' '));
 					var tmpNode = revNode.cloneNode(true);
-					tmpNode.firstChild.setAttribute('href', href + '&' + MorebitsGlobal.queryString.create({ 'twinklerevert': 'norm' }));
+					tmpNode.firstChild.setAttribute('href', href + '&twinklerevert=norm');
 					current.appendChild(tmpNode);
 					current.appendChild(document.createTextNode(' '));
 					tmpNode = revVandNode.cloneNode(true);
-					tmpNode.firstChild.setAttribute('href', href + '&' + MorebitsGlobal.queryString.create({ 'twinklerevert': 'vand' }));
+					tmpNode.firstChild.setAttribute('href', href + '&twinklerevert=vand');
 					current.appendChild(tmpNode);
 				});
 			}
@@ -483,16 +483,16 @@ TwinkleGlobal.fluff.callbacks = {
 
 			switch (TwinkleGlobal.getPref('userTalkPageMode')) {
 				case 'tab':
-					window.open(mw.util.wikiScript('index') + '?' + MorebitsGlobal.queryString.create(query), '_blank');
+					window.open(mw.util.getUrl('', query), '_blank');
 					break;
 				case 'blank':
-					window.open(mw.util.wikiScript('index') + '?' + MorebitsGlobal.queryString.create(query), '_blank',
+					window.open(mw.util.getUrl('', query), '_blank',
 						'location=no,toolbar=no,status=no,directories=no,scrollbars=yes,width=1200,height=800');
 					break;
 				case 'window':
 				/* falls through */
 				default:
-					window.open(mw.util.wikiScript('index') + '?' + MorebitsGlobal.queryString.create(query),
+					window.open(mw.util.getUrl('', query),
 						window.name === 'twinklewarnwindow' ? '_blank' : 'twinklewarnwindow',
 						'location=no,toolbar=no,status=no,directories=no,scrollbars=yes,width=1200,height=800');
 					break;

modules/twinklewarn.js
@@ -101,7 +101,7 @@ TwinkleGlobal.warn.callback = function twinklewarnCallback() {
 		type: 'input',
 		name: 'article',
 		label: 'Linked page',
-		value: MorebitsGlobal.queryString.exists('vanarticle') ? MorebitsGlobal.queryString.get('vanarticle') : '',
+		value: mw.util.getParamValue('vanarticle') || '',
 		tooltip: 'A page can be linked within the notice, perhaps because it was a revert to said page that dispatched this notice. Leave empty for no page to be linked.'
 	});
 

morebits.js
@@ -1404,12 +1404,21 @@ MorebitsGlobal.wiki.api.prototype = {
 
 		++MorebitsGlobal.wiki.numberOfActionsLeft;
 
+		var queryString = $.map(this.query, function(val, i) {
+			if (Array.isArray(val)) {
+				return encodeURIComponent(i) + '=' + val.map(encodeURIComponent).join('|');
+			} else if (val !== undefined) {
+				return encodeURIComponent(i) + '=' + encodeURIComponent(val);
+			}
+		}).join('&').replace(/^(.*?)(\btoken=[^&]*)&(.*)/, '$1$3&$2');
+		// token should always be the last item in the query string (bug TW-B-0013)
+
 		var ajaxparams = $.extend({}, {
 			context: this,
 			type: 'POST',
 			url: mw.util.wikiScript('api'),
-			data: MorebitsGlobal.queryString.create(this.query),
-			dataType: this.query.format,
+			data: queryString,
+			dataType: 'xml',
 			headers: {
 				'Api-User-Agent': morebitsWikiApiUserAgent
 			}
@@ -3447,147 +3456,6 @@ MorebitsGlobal.wikitext.page.prototype = {
 	}
 };
 
-
-
-/**
- * **************** MorebitsGlobal.queryString ****************
- * Maps the querystring to an JS object
- *
- * In static context, the value of location.search.substring(1), else the value given
- * to the constructor is going to be used. The mapped hash is saved in the object.
- *
- * Example:
- *
- * var value = MorebitsGlobal.queryString.get('key');
- * var obj = new MorebitsGlobal.queryString('foo=bar&baz=quux');
- * value = obj.get('foo');
- */
-
-/**
- * @constructor
- * @param {string} qString   The query string
- */
-MorebitsGlobal.queryString = function QueryString(qString) {
-	this.string = qString;
-	this.params = {};
-
-	if (!qString.length) {
-		return;
-	}
-
-	qString = qString.replace(/\+/g, ' ');
-	var args = qString.split('&');
-
-	for (var i = 0; i < args.length; ++i) {
-		var pair = args[i].split('=');
-		var key = decodeURIComponent(pair[0]), value = key;
-
-		if (pair.length === 2) {
-			value = decodeURIComponent(pair[1]);
-		}
-
-		this.params[key] = value;
-	}
-};
-
-MorebitsGlobal.queryString.staticstr = null;
-
-MorebitsGlobal.queryString.staticInit = function() {
-	if (!MorebitsGlobal.queryString.staticstr) {
-		MorebitsGlobal.queryString.staticstr = new MorebitsGlobal.queryString(location.search.substring(1));
-	}
-};
-
-MorebitsGlobal.queryString.get = function(key) {
-	MorebitsGlobal.queryString.staticInit();
-	return MorebitsGlobal.queryString.staticstr.get(key);
-};
-
-MorebitsGlobal.queryString.exists = function(key) {
-	MorebitsGlobal.queryString.staticInit();
-	return MorebitsGlobal.queryString.staticstr.exists(key);
-};
-
-MorebitsGlobal.queryString.equals = function(key, value) {
-	MorebitsGlobal.queryString.staticInit();
-	return MorebitsGlobal.queryString.staticstr.equals(key, value);
-};
-
-MorebitsGlobal.queryString.toString = function() {
-	MorebitsGlobal.queryString.staticInit();
-	return MorebitsGlobal.queryString.staticstr.toString();
-};
-
-MorebitsGlobal.queryString.create = function(arr) {
-	var resarr = [];
-	var editToken;  // KLUGE: this should always be the last item in the query string (bug TW-B-0013)
-	for (var i in arr) {
-		if (arr[i] === undefined) {
-			continue;
-		}
-		var res;
-		if (Array.isArray(arr[i])) {
-			var v = [];
-			for (var j = 0; j < arr[i].length; ++j) {
-				v[j] = encodeURIComponent(arr[i][j]);
-			}
-			res = v.join('|');
-		} else {
-			res = encodeURIComponent(arr[i]);
-		}
-		if (i === 'token') {
-			editToken = res;
-		} else {
-			resarr.push(encodeURIComponent(i) + '=' + res);
-		}
-	}
-	if (editToken !== undefined) {
-		resarr.push('token=' + editToken);
-	}
-	return resarr.join('&');
-};
-
-/**
- * @returns {string} the value associated to the given `key`
- * @param {string} key
- */
-MorebitsGlobal.queryString.prototype.get = function(key) {
-	return this.params[key] || null;
-};
-
-/**
- * @returns {boolean} true if the given `key` is set
- * @param {string} key
- */
-MorebitsGlobal.queryString.prototype.exists = function(key) {
-	return !!this.params[key];
-};
-
-/**
- * @returns {boolean} true if the value associated with given `key` equals given `value`
- * @param {string} key
- * @param {string} value
- */
-MorebitsGlobal.queryString.prototype.equals = function(key, value) {
-	return this.params[key] === value;
-};
-
-/**
- * @returns {string}
- */
-MorebitsGlobal.queryString.prototype.toString = function() {
-	return this.string || null;
-};
-
-/**
- * Creates a querystring and encodes strings via `encodeURIComponent` and joins arrays with `|`
- * @param {Array} arr
- * @returns {string}
- */
-MorebitsGlobal.queryString.prototype.create = MorebitsGlobal.queryString.create;
-
-
-
 /**
  * **************** MorebitsGlobal.status ****************
  */
@@ -4418,7 +4286,6 @@ if (typeof arguments === 'undefined') {  // typeof is here for a reason...
 	window.QuickForm = MorebitsGlobal.quickForm;
 	window.Wikipedia = MorebitsGlobal.wiki;
 	window.Status = MorebitsGlobal.status;
-	window.QueryString = MorebitsGlobal.queryString;
 }
 
 // </nowiki>

commit 09dd298a9b9daf5a516a34348f701691e9779ba8

diff: Restore diffDisabledWikis
Fix bug introduced in 736d3ba61d97f1738585c0fde667ea2fe289aec3.

modules/twinklediff.js
@@ -16,6 +16,14 @@ TwinkleGlobal.diff = function twinklediff() {
 	if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
 		return;
 	}
+
+	var disabledWikis = $.map(TwinkleGlobal.getPref('diffDisabledWikis'), function(el) {
+		return el.value.trim();
+	});
+	if (disabledWikis.indexOf(mw.config.get('wgDBname')) !== -1) {
+		return;
+	}
+
 	TwinkleGlobal.addPortletLink(mw.util.getUrl(mw.config.get('wgPageName'), {diff: 'cur', oldid: 'prev'}), 'Last', 'tw-lastdiff', 'Show most recent diff');
 
 	// Show additional tabs only on diff pages